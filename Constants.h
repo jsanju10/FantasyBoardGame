4ce639f84252ab868e6e855352cc2d02a962761a
Nikki <49493685+navjotpuar@users.noreply.github.com>
2025-11-16 14:49:16 +0000

    Update Constants.h

--- Constants.h
+++ Constants.h
@@ -1,17 +1,15 @@
-/**
- * @file Constants.h
- * @brief Defines global constants and race statistics for the Fantasy Game.
- */
+// Constants.h
+// Stores game configuration values and race stats.
 
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
 namespace Constants {
 
-    // Number of commands before switching day/night
-    constexpr int COMMANDS_PER_TIME_SWITCH = 5;
+    // Switch day/night after this many commands
+    const int COMMANDS_PER_TIME_SWITCH = 5;
 
-    // Race statistics structure
+    // Structure for race stats
     struct RaceStats {
         int attack;
         double attackChance;
@@ -21,17 +19,16 @@ namespace Constants {
         int strength;
     };
 
-    // Predefined race stats
-    constexpr RaceStats HUMAN  = {30, 2.0/3.0, 20, 0.5, 60, 100};
-    constexpr RaceStats ELF    = {40, 1.0,     10, 0.25, 40, 70};
-    constexpr RaceStats DWARF  = {30, 2.0/3.0, 20, 2.0/3.0, 50, 130};
-    constexpr RaceStats HOBBIT = {25, 1.0/3.0, 20, 2.0/3.0, 70, 85};
+    // Race definitions
+    const RaceStats HUMAN  = {30, 0.66, 20, 0.50, 60, 100};
+    const RaceStats ELF    = {40, 1.00, 10, 0.25, 40, 70};
+    const RaceStats DWARF  = {30, 0.66, 20, 0.66, 50, 130};
+    const RaceStats HOBBIT = {25, 0.33, 20, 0.66, 70, 85};
 
     // Orc stats differ by time of day
-    constexpr RaceStats ORC_DAY   = {25, 0.25, 10, 0.25, 50, 130};
-    constexpr RaceStats ORC_NIGHT = {45, 1.0,  25, 0.5,  50, 130};
+    const RaceStats ORC_DAY   = {25, 0.25, 10, 0.25, 50, 130};
+    const RaceStats ORC_NIGHT = {45, 1.00, 25, 0.50, 50, 130};
 
 } // namespace Constants
 
-#endif // CONSTANTS_H
 

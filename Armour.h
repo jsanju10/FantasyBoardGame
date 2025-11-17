65b5d9f3063c6528b0b000a2716477c9db7dc543
Denizcalik00 <denizcalik00@gmail.com>
2025-11-17 16:48:13 +0000

    Update Armour.h

--- Armour.h
+++ Armour.h
@@ -1,4 +1,28 @@
 #ifndef ARMOUR_H
 #define ARMOUR_H
 
+
+
+#include "Item.h"
+
+// Armour give defence but maybe reduce attack
+class Armour : public Item
+{
+private:
+    int defenceBoost;
+    int attackPenalty;
+
+public:
+    Armour(std::string n, int w, int defBoost, int atkPenalty)
+        : Item(n, w, ARMOUR)
+    {
+        defenceBoost = defBoost;
+        attackPenalty = atkPenalty;
+    }
+
+    void applyEffect(Character &c);
+    void removeEffect(Character &c);
+};
+
+
 #endif // ARMOUR_H
